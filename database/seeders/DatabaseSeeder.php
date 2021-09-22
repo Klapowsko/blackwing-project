2dda2eb7bbf5804af941e52528300e64cac1a754
hiago <hiago.klapowsko@nerdweb.com.br>
2021-09-22 14:15:22 -0300

    atualizado seeder de states

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            DemandsSeeder::class,
+            DemandsStatusSeeder::class,
+            PartsSeeder::class,
+            StatesSeeder::class,
+            UsersSeeder::class,
+            UsersTypeSeeder::class
+        ]);
     }
 }
